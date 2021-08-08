f43228a7339478bff9721ac4637d6497d71b973e
Matt <mgroe@ieee.org>
2021-08-07 17:24:00 -0700

    Remove is working for sprints and displaying properly in the table view. Refs #19

--- src/components/ImportData.js
+++ src/components/ImportData.js
@@ -5,7 +5,7 @@ import Papa from 'papaparse';
 
 //  Redux Imports
 import { connect } from 'react-redux';
-import { set } from '../redux/data/sprintSlice';
+import { add, remove } from '../redux/data/sprintSlice';
 
 // CSS
 import '../assets/css/pam.css';
@@ -15,7 +15,7 @@ class ImportData extends React.Component {
     super(props);
     this.state = {
       selectedCSV: null,
-      arrayCSV: []
+      arrayCSV: [],
     };
     this.setJson = this.setJson.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
@@ -32,15 +32,21 @@ class ImportData extends React.Component {
   onHandleSubmit(e) {
     e.preventDefault();
     // Add csv to array
-    let arr = this.state.arrayCSV;
+    let arr = [...this.state.arrayCSV];
     arr.push(this.state.selectedCSV);
     console.log("CSV array: ", arr)
-    this.setState({arrayCSV: arr}, () => {
+    this.setState({arrayCSV: arr, team: null, sprint: null}, () => {
       // Call on Papa parse to extract the csv as a json callback to setJson() function
       Papa.parse(this.state.selectedCSV, {
         complete: this.setJson
       });
     });
+  }
+
+  removeCSV(UID) {
+    // passing the UID for the respective sprint data / csv
+    console.log("UID: ", UID);
+    this.props.remove(UID);
   }
 
   setJson(result) {
@@ -50,17 +56,23 @@ class ImportData extends React.Component {
     let sprint = titleArray[4];
     let sprintObject = {
       name: team,
+      UID: team+sprint,
       sprint: sprint,
       issues: result.data
     }
     // this resets the file input (utilizes "ref" tag and the "file_input_file" id)
     this.fileInput.value = "";
     // call the SET action for sprints to add the uploaded sprint to the users data
-    this.props.set(sprintObject);
+    this.props.add(sprintObject);
+  }
+
+  componentDidUpdate() {
+    console.log("New props: ", this.props.sprints)
   }
 
   render() {
-    const { arrayCSV } = this.state;
+    const { arrayCSV, team, sprint } = this.state;
+    const { sprints } = this.props;
 
     return(
       <div>
@@ -68,13 +80,13 @@ class ImportData extends React.Component {
           <label htmlFor="formFile" className="form-label mt-4">Choose a CSV file for import</label>
           <div className="csv-input-group">
             <input id="fileInput" className="form-control" type="file" ref={ref=> this.fileInput = ref} onChange={(e) => this.getCSV(e)}/>
-            <button type="button" className="btn btn-light" onClick={this.onHandleSubmit}>
+            <button id="submitCSVButton" type="button" className="btn btn-light" onClick={this.onHandleSubmit}>
               Submit
             </button>
           </div>
         </div>
         {
-          arrayCSV ?
+          sprints ?
             <div>
                   {/* THIS BLOCK SHOULD BECOME A COMPONENT THAT WE PASS THE CSV ARRAY TO */}
                 <table className="table table-hover">
@@ -87,12 +99,16 @@ class ImportData extends React.Component {
                     </tr>
                   </thead>
                   <tbody>
-                    {arrayCSV.map(csv => (
-                      <tr className="table-light">
-                        <th scope="row">{csv.name}</th>
-                        <td>View</td>
+                    {sprints.data.map(sprint => (
+                      <tr key={sprint.UID} className="table-light">
+                        <th scope="row">{sprint.Project + " Sprint " + sprint.Sprint }</th>
+                        <td>
+                          <button type="button" className="btn btn-light">View</button>
+                        </td>
                         <td>Edit</td>
-                        <td>Remove</td>
+                        <td>
+                          <button id="removeCSVButton" type="button" className="btn btn-light" onClick={() => this.removeCSV(sprint.UID)}>Remove</button>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -111,6 +127,6 @@ const mapStateToProps = (state) => ({
   sprints: state.sprints
 });
 
-const mapDispatchToProps = { set };
+const mapDispatchToProps = { add, remove };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ImportData);
--- src/redux/data/sprintSlice.js
+++ src/redux/data/sprintSlice.js
@@ -2,53 +2,76 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const sprintSlice = createSlice({
     name: 'sprint data',
-    initialState: { },
+    initialState: {
+        data: []
+    },
     reducers: {
-        set: (state, action) => {
-            console.log("The payload: ", action.payload);
+        add: (state, action) => {
             /*
-            * SET method
+            * ADD method
             *     saving the passed sprint data into the redux store
             **/
-            // let sprintObject = {};
-            // let pointsAchieved = 0;
-            // let issueArray = [];
-            // let issueAttributes = [];
-            // let arraySize = action.payload.issues.length;
-            // action.payload.issues.forEach((issue, index) => {
-            //     //  if this is the first row in the array, then we need to extract the object data from it.
-            //     if (index === 0) {
-            //         // iterate through the issue array (this contains the titles of each column)
-            //         issue.forEach((attribute) => {
-            //             //  push to attributes (i.e. the titles of each column of data) array
-            //             issueAttributes.push(attribute);
-            //         });
-            //         // We have to stop at Array - 1 since the csv's generated have excess at the end.
-            //     } else if (index < arraySize-1) {
-            //         // Instantiate issueObject that contains all data pertanent to an issue (i.e. title, description.. etc)
-            //         let issueObject = {};
-            //         issue.forEach((data, index) => {
-            //             let objectAttribute = issueAttributes[index]
-            //             issueObject[objectAttribute] = data;
-            //             if (objectAttribute === "Story Points") {
-            //                 console.log(data)
-            //                 pointsAchieved += parseInt(data);
-            //             }
-            //         });
-            //         issueArray.push(issueObject);
-            //     }
-            // });
-            // // Create Sprint Object - (i.e. Name of Project, Sprint #, Issues Completed, Points Achieved)
-            // sprintObject['Project'] = action.payload.name;
-            // sprintObject['Sprint'] = action.payload.sprint;
-            // sprintObject['Issues'] = [...issueArray];
-            // sprintObject['IssuesCompleted'] = issueArray.length;
-            // sprintObject['PointsAchieved'] = pointsAchieved;
-            // state.data = sprintObject;
+            let sprintObject = {};
+            let pointsAchieved = 0;
+            let issueArray = [];
+            let issueAttributes = [];
+            let arraySize = action.payload.issues.length;
+            action.payload.issues.forEach((issue, index) => {
+                //  if this is the first row in the array, then we need to extract the object data from it.
+                if (index === 0) {
+                    // iterate through the issue array (this contains the titles of each column)
+                    issue.forEach((attribute) => {
+                        //  push to attributes (i.e. the titles of each column of data) array
+                        issueAttributes.push(attribute);
+                    });
+                    // We have to stop at Array - 1 since the csv's generated have excess at the end.
+                } else if (index < arraySize-1) {
+                    // Instantiate issueObject that contains all data pertanent to an issue (i.e. title, description.. etc)
+                    let issueObject = {};
+                    issue.forEach((data, index) => {
+                        let objectAttribute = issueAttributes[index]
+                        issueObject[objectAttribute] = data;
+                        if (objectAttribute === "Story Points") {
+                            console.log(data)
+                            pointsAchieved += parseInt(data);
+                        }
+                    });
+                    issueArray.push(issueObject);
+                }
+            });
+            // Create Sprint Object - (i.e. Name of Project, Sprint #, Issues Completed, Points Achieved)
+            sprintObject['Project'] = action.payload.name;
+            sprintObject['Sprint'] = action.payload.sprint;
+            sprintObject['Issues'] = [...issueArray];
+            sprintObject['IssuesCompleted'] = issueArray.length;
+            sprintObject['PointsAchieved'] = pointsAchieved;
+
+            // Creating node Unique Identifier
+            sprintObject['UID'] = sprintObject['Project'] + sprintObject['Sprint'];
+
+            // Add the previous data to the array before adding the new one.
+            let data = state.data
+            data.push(sprintObject)
+
+            // Set the new state data
+            state.data = data;
+        },
+        remove: (state, action) => {
+            console.log("Remove action triggered (pre-state): ", state.data);
+            let sprintIndex = 0;
+            state.data.forEach((sprint, index) => {
+                if (sprint.UID === action.payload) {
+                    console.log(`Sprint ${action.payload} found.`);
+                    sprintIndex=index;
+                }
+            });
+            let array = state.data;
+            array.splice(sprintIndex, 1);
+            console.log("Resultant data after removal: ", array)
         }
     }
 })
 
-export const { set } = sprintSlice.actions;
+export const { add, remove } = sprintSlice.actions;
 
 export default sprintSlice.reducer;
